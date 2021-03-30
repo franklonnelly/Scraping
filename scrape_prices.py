0a804975af68e28ea0864ba9e6802b4902cfdcc7
franklonnelly <50146371+franklonnelly@users.noreply.github.com>
2021-03-30 15:22:47 +0200

    Update scrape_prices.py

--- scrape_prices.py
+++ scrape_prices.py
@@ -6,12 +6,9 @@ import pandas as pd
 import csv
 
 source_url = 'https://www.eia.gov/dnav/pet/pet_pri_gnd_dcus_sny_w.htm'
-# URL with MONTHLY data selection grouped by AREA
+# URL with monthly data selection grouped by area
 gas_url = 'https://www.eia.gov/dnav/pet/pet_pri_gnd_a_epm0_pte_dpgal_m.htm'
 diesel_url = 'https://www.eia.gov/dnav/pet/pet_pri_gnd_a_epd2d_pte_dpgal_m.htm'
-
-
-"""Connect to URL and parse HTML code"""
 
 def read_url_html(url):
     r = requests.get(url)
@@ -23,9 +20,6 @@ def read_url_html(url):
     # or html=urlopen(m_area_url)
     print('Dataset name: {}'.format(soup.title.text))
     return soup
-
-
-"""Locate all data source and export data in padas dataframes (appended)"""
 
 def find_all_data(url):
     soup = read_url_html(url)
@@ -57,25 +51,50 @@ def find_all_data(url):
 gas_df = find_all_data(gas_url)
 diesel_df = find_all_data(diesel_url)
 
+"""Save results"""
 
-""" Locate all data source and export data in padas dataframes (appended) """
+diesel_csv = r'C:\Users\Francesco.Sollitto\Desktop\Diesel_Prices.csv'
+gas_csv = r'C:\Users\Francesco.Sollitto\Desktop\Gas_Prices.csv'
+price_var = r'C:\Users\Francesco.Sollitto\Desktop\Price_variations.csv'
+min_trimester = r'C:\Users\Francesco.Sollitto\Desktop\Min_state.txt'
 
+diesel_df.to_csv(diesel_csv, index=False)
+gas_df.to_csv(gas_csv, index=False)
+
+
+"""Statistics per State"""
 # states = ['California','Colorado','Florida','Massachusetts','Minnesota',
 #          'New York','Ohio','Texas','Washington']
 
-def state_variations(df_name=gas_df):
-    for index, item in df_name.iterrows():
-        output = ''
-        if item['Location'] in states:
-            state = item['Location']
-            min_value = df_name.loc[df_name['Location'] == state, 'Value'].min()
-            max_value = df_name.loc[df_name['Location'] == state, 'Value'].max()
-            variation = min_value - max_value
-            # Csv write row
-            output = '{},{},{},{}'.format(item['Location'], min_value, max_value, variation)
-    return output
+def state_variations(gas_df):
+    df_states = gas_df.loc[gas_df['Aggregation']=='States']
+    df_st = df_states.drop_duplicates(['Location'])
+    state_list = df_st['Location'].tolist()
+    output=''
+    for st in state_list:
+        min_value = df_states.loc[(df_states['Location'] == st) & (df_states['Period']>202100), 'Value'].min()
+        max_value = df_states.loc[(df_states['Location'] == st) & (df_states['Period']>202100), 'Value'].max()
+        variation = abs(round((min_value - max_value),3))
+        output += '{};{}\n'.format(st, variation)
+        # Csv write row
+    with open(price_var, 'w+') as pv:
+        pv.write(output)
 
 
-"""Save results"""
-diesel_csv = r'C:\Users\Francesco.Sollitto\Desktop\Diesel_Prices.csv'
-diesel_df.to_csv(diesel_csv, index=False)
+def state_min_avglast_3months(gas_df):
+    df_states = gas_df.loc[gas_df['Aggregation']=='States']
+    df_st = df_states.drop_duplicates(['Location'])
+    state_list = df_st['Location'].tolist()
+    df_means = pd.DataFrame()
+    for st in state_list:
+        df_trim = df_states.loc[(df_states['Location'] == st) & (df_states['Period']>202100)]
+        df_mean = df_trim.groupby(['Location'], as_index=False).mean()
+        df_means = df_means.append(df_mean)
+    state_min = df_means[df_means.Value == df_means.Value.min()]
+    # min_value = round(df_means['Value'].min(),3)    
+    output = '{},{}'.format(state_min.iloc[0]['Location'], round(state_min.iloc[0]['Value'], 2))
+    with open(min_trimester, 'w+') as mt:
+        mt.write(output)
+        
+        
+        
